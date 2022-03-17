146e5cd0add672f1c3e1f4e9f02fb79d48a14a77
SvenKim <svenkim_zhuo@qq.com>
2022-03-17 21:26:38 +0800

    support less or more framesize

--- test.cpp
+++ test.cpp
@@ -3,12 +3,14 @@
  *
  *  AUTHORS:       zhuocandong    START DATE: Wednesday March 16th 2022
  *
- *  LAST MODIFIED: Thursday, March 17th 2022, 10:29:16 am
+ *  LAST MODIFIED: Thursday, March 17th 2022, 5:45:07 pm
  *
  *  CONTACT:       candong.zhuo@smartmore.com
-*******************************************************************************/
+ *******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+
+#define FAB(a, b) ((a > b) ? (a - b) : (b - a))
 
 struct fifo
 {
@@ -48,40 +50,63 @@ int main(int argc, char **argv)
         }
     }
     int _max_conventions = in_t; // the maximum number of conventions
+    int _in_factor = in_nb / _max_conventions;
+    int _out_factor = out_nb / _max_conventions;
+    int plus_num_ = 0;
+    int minus_num_ = 0;
 
-    while (counts < 30)
+    while (counts < atoi(argv[3]))
     {
         int in_pts = counts++ * in_nb;
-        int plus_num = 0;
         foo.size += in_nb;
-        while (audio_fifo_size(foo) >= out_nb)
+
+        do // FlushFifo( )
         {
-
-            ReadFrameFromFifo(foo, out_nb);
-            int out_pts = 0;
-
-            int factor = out_nb / _max_conventions;
-
             int in_num = in_pts / in_nb;
 
             if (!_b_got_first_frame)
             {
-                if ((in_num % factor) != 0)
+                if ((in_num % _out_factor) != 0)
                 {
-                    break;
+                    continue;
                 }
                 _b_got_first_frame = true;
             }
+            else if (_b_got_first_frame && (in_num % _out_factor) == 0)
+            {
+                // reset plus_num and minuns_num
+                plus_num_ = 0;
+                minus_num_ = 0;
+            }
 
-            in_num += (in_num / factor) + plus_num;
+            if (audio_fifo_size(foo) < out_nb)
+            {
+                minus_num_ += 1;
+            }
 
-            out_pts = in_num * out_nb;
+            while (audio_fifo_size(foo) >= out_nb)
+            {
 
-            printf("[%d]end fifo_size:%d, \t\t[%d]OutputPTS:%d\n", in_pts / in_nb, audio_fifo_size(foo), out_pts / out_nb, out_pts);
-            // encode(_ofmt_ctx, _output_codec_context, tmp_frame, _output_stream_index);
-            // av_frame_free(&tmp_frame);
-            if (audio_fifo_size(foo) == out_nb)
-                plus_num = (in_nb - out_nb) / _max_conventions;
-        }
+                ReadFrameFromFifo(foo, out_nb);
+
+                int out_num;
+                if (in_nb > out_nb)
+                    out_num = in_num + (in_num / _out_factor) * FAB(_in_factor, _out_factor) + plus_num_;
+                else if (in_nb < out_nb)
+                    out_num = in_num - (in_num / _out_factor) * FAB(_in_factor, _out_factor) - minus_num_;
+                else
+                    out_num = in_num;
+
+                int out_pts = out_num * out_nb;
+
+                if (audio_fifo_size(foo) >= out_nb)
+                {
+                    plus_num_ += 1;
+                }
+
+                printf("[%d]end fifo_size:%d, \t\t[%d]OutputPTS:%d\n", in_pts / in_nb, audio_fifo_size(foo), out_pts / out_nb, out_pts);
+                // encode(_ofmt_ctx, _output_codec_context, tmp_frame, _output_stream_index);
+            }
+        } while (0);
     }
 }
